c59a1da8e195929f845dca1d18534c7e0862b4b7
Tokarev Artem Alekseevich <tokarev28.art@gmail.com>
2019-06-13 16:01:14 +0500

    update

--- polym-orient-obj/main.cpp
+++ polym-orient-obj/main.cpp
@@ -1,18 +1,16 @@
+#include <cstddef>
 #include <iostream>
 #include <algorithm>
+#include <array>
 #include <string>
-#include <ctime>
+#include <chrono>
+#include <optional>
 #define TINYOBJLOADER_IMPLEMENTATION
 #include "tiny_obj_loader.h"
 #include "vec.h"
 
 
 using indices = std::vector<tinyobj::index_t>;
-using spt::vec3;
-
-
-#define TIME_TO_MS(time_t_var) \
-    (time_t_var * 1000 / CLOCKS_PER_SEC)
 
 
 bool faceContainsVert( indices::const_iterator face, indices::const_iterator vert )
@@ -26,28 +24,28 @@ bool faceContainsVert( indices::const_iterator face, indices::const_iterator ver
 }
 
 
-bool findOneVertNot( int& out, indices::const_iterator inFace, indices::const_iterator notFace )
+bool findOneVertNot(std::size_t& out, indices::const_iterator in_face, indices::const_iterator not_face)
 {
-    int n_not_conts = 0;
-    bool not_conts[3];
-    if (not_conts[0] = !faceContainsVert(notFace, inFace + 0)) n_not_conts++;
-    if (not_conts[1] = !faceContainsVert(notFace, inFace + 1)) n_not_conts++;
-    if (not_conts[2] = !faceContainsVert(notFace, inFace + 2)) n_not_conts++;
+    std::size_t n_not_conts = 0;
+    std::array<bool, 3> not_conts;
+    if (not_conts[0] = !faceContainsVert(not_face, in_face + 0)) n_not_conts++;
+    if (not_conts[1] = !faceContainsVert(not_face, in_face + 1)) n_not_conts++;
+    if (not_conts[2] = !faceContainsVert(not_face, in_face + 2)) n_not_conts++;
     if (n_not_conts != 1) return false;
 
     if (not_conts[0])
     {
-        out = (inFace + 0)->vertex_idx;
+        out = (in_face + 0)->vertex_idx;
         return true;
     }
     if (not_conts[1])
     {
-        out = (inFace + 1)->vertex_idx;
+        out = (in_face + 1)->vertex_idx;
         return true;
     }
     if (not_conts[2])
     {
-        out = (inFace + 2)->vertex_idx;
+        out = (in_face + 2)->vertex_idx;
         return true;
     }
 
@@ -55,72 +53,74 @@ bool findOneVertNot( int& out, indices::const_iterator inFace, indices::const_it
 }
 
 
-int findOppVert( indices::const_iterator face, const tinyobj::shape_t& shape )
+std::optional<std::size_t> opp_vert(indices::const_iterator face, const tinyobj::shape_t& shape)
 {
-    std::vector<int> opp_idces;
+    std::vector<std::size_t> opp_idces;
 
-    for (size_t i = 0; i < shape.mesh.indices.size(); i += 3)
+    for (std::size_t i = 0; i < shape.mesh.indices.size(); i += 3)
     {
-        int opp_idx;
+        std::size_t opp_idx;
         if (findOneVertNot(opp_idx, shape.mesh.indices.begin() + i, face))
         {
             if (std::count(opp_idces.begin(), opp_idces.end(), opp_idx) == 2)
-                return opp_idx;
+                return std::optional(opp_idx);
 
             opp_idces.push_back(opp_idx);
         }
     }
 
-    return -1;
+    return std::nullopt;
 }
 
 
-vec3 getVertPos( int idx, const tinyobj::attrib_t& attrib )
+spt::vec3 vert_pos(std::size_t idx, const tinyobj::attrib_t& attrib)
 {
-    return vec3(attrib.vertices[3 * idx + 0],
-                attrib.vertices[3 * idx + 1],
-                attrib.vertices[3 * idx + 2]);
+    return spt::vec3(attrib.vertices[3 * idx + 0],
+                     attrib.vertices[3 * idx + 1],
+                     attrib.vertices[3 * idx + 2]);
 }
 
 
-vec3 computeRawNormal( indices::const_iterator face, const tinyobj::attrib_t& attrib )
+spt::vec3 raw_normal(indices::const_iterator face, const tinyobj::attrib_t& attrib)
 {
-    vec3 v0(attrib.vertices[3 * (face + 0)->vertex_idx + 0],
-            attrib.vertices[3 * (face + 0)->vertex_idx + 1],
-            attrib.vertices[3 * (face + 0)->vertex_idx + 2]);
-    vec3 v1(attrib.vertices[3 * (face + 1)->vertex_idx + 0],
-            attrib.vertices[3 * (face + 1)->vertex_idx + 1],
-            attrib.vertices[3 * (face + 1)->vertex_idx + 2]);
-    vec3 v2(attrib.vertices[3 * (face + 2)->vertex_idx + 0],
-            attrib.vertices[3 * (face + 2)->vertex_idx + 1],
-            attrib.vertices[3 * (face + 2)->vertex_idx + 2]);
+    spt::vec3 v0(attrib.vertices[3 * (face + 0)->vertex_idx + 0],
+                 attrib.vertices[3 * (face + 0)->vertex_idx + 1],
+                 attrib.vertices[3 * (face + 0)->vertex_idx + 2]);
 
-    return vec3::cross(v1 - v0, v2 - v0);
+    spt::vec3 v1(attrib.vertices[3 * (face + 1)->vertex_idx + 0],
+                 attrib.vertices[3 * (face + 1)->vertex_idx + 1],
+                 attrib.vertices[3 * (face + 1)->vertex_idx + 2]);
+
+    spt::vec3 v2(attrib.vertices[3 * (face + 2)->vertex_idx + 0],
+                 attrib.vertices[3 * (face + 2)->vertex_idx + 1],
+                 attrib.vertices[3 * (face + 2)->vertex_idx + 2]);
+
+    return spt::vec3::cross(v1 - v0, v2 - v0);
 }
 
 
-void orientateMesh( const tinyobj::attrib_t& attrib, tinyobj::shape_t& shape, std::vector<size_t>& dontWriteFaces )
+void orientate_mesh(const tinyobj::attrib_t& attrib, tinyobj::shape_t& shape, std::vector<size_t>& dont_write_faces)
 {
     std::vector<std::pair<indices::iterator, indices::iterator>> indices_to_swap;
 
     size_t n_indices = shape.mesh.indices.size();
     #pragma omp parallel for
-    for (size_t idx_offset = 0; idx_offset < n_indices; idx_offset += 3)
+    for (std::size_t idx_offset = 0; idx_offset < n_indices; idx_offset += 3)
     {
-        int opp_vert_idx = findOppVert(shape.mesh.indices.begin() + idx_offset, shape);
-        if (opp_vert_idx == -1)
+        auto opp_vert_idx = opp_vert(shape.mesh.indices.begin() + idx_offset, shape);
+        if (!opp_vert_idx)
             #pragma omp critical
-            dontWriteFaces.push_back(idx_offset);
-        vec3 opp_vert_pos = getVertPos(opp_vert_idx, attrib);
+            dont_write_faces.push_back(idx_offset);
+        spt::vec3 opp_vert_pos = vert_pos(*opp_vert_idx, attrib);
 
-        vec3 v0_to_opp = opp_vert_pos - vec3(attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 0],
-                                             attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 1],
-                                             attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 2]);
+        spt::vec3 v0_to_opp = opp_vert_pos - spt::vec3(attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 0],
+                                                       attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 1],
+                                                       attrib.vertices[3 * shape.mesh.indices[idx_offset].vertex_idx + 2]);
 
-        vec3 raw_normal = computeRawNormal(shape.mesh.indices.begin() + idx_offset, attrib);
+        spt::vec3 raw_normal_l = raw_normal(shape.mesh.indices.begin() + idx_offset, attrib);
 
-        float dot_val = vec3::dot(v0_to_opp, raw_normal);
-        if (dot_val > 0.0f)
+        spt::vec3::real_t dot_val = spt::vec3::dot(v0_to_opp, raw_normal_l);
+        if (dot_val > 0.0)
             #pragma omp critical
             indices_to_swap.push_back({ shape.mesh.indices.begin() + idx_offset + 1,
                                         shape.mesh.indices.begin() + idx_offset + 2 });
@@ -131,11 +131,12 @@ void orientateMesh( const tinyobj::attrib_t& attrib, tinyobj::shape_t& shape, st
 }
 
 
-void writeFile( const tinyobj::attrib_t& attrib, const tinyobj::shape_t& shape, std::vector<size_t>& dontWriteFaces, std::string_view filename )
+void write_file(const tinyobj::attrib_t& attrib, const tinyobj::shape_t& shape,
+                std::vector<size_t>& dont_write_faces, std::string_view filename)
 {
     std::ofstream file(filename.data());
 
-    for (size_t i = 0; i < attrib.vertices.size(); i += 3)
+    for (std::size_t i = 0; i < attrib.vertices.size(); i += 3)
     {
         file << "v "
              << attrib.vertices[i + 0] << ' '
@@ -143,9 +144,9 @@ void writeFile( const tinyobj::attrib_t& attrib, const tinyobj::shape_t& shape, 
              << attrib.vertices[i + 2] << '\n';
     }
 
-    for (size_t idx_offset = 0; idx_offset < shape.mesh.indices.size(); idx_offset += 3)
+    for (std::size_t idx_offset = 0; idx_offset < shape.mesh.indices.size(); idx_offset += 3)
     {
-        if (std::find(dontWriteFaces.begin(), dontWriteFaces.end(), idx_offset) != dontWriteFaces.end())
+        if (std::find(dont_write_faces.begin(), dont_write_faces.end(), idx_offset) != dont_write_faces.end())
             continue;
 
         file << "f "
@@ -166,10 +167,11 @@ int main()
 
     std::string warn;
     std::string err;
-    time_t start = clock();
+    auto start = std::chrono::steady_clock::now();
     bool ret = tinyobj::load_obj(&attrib, &shapes, &materials, &warn, &err, filename);
-    time_t elapsed = clock() - start;
-    std::cout << "Loading time: " << TIME_TO_MS(elapsed) << " ms\n";
+    auto stop = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = stop - start;
+    std::cout << "Loading time: " << elapsed.count() << " s\n";
 
     if (!err.empty())
         std::cerr << err << std::endl;
@@ -178,12 +180,13 @@ int main()
         return 1;
 
     std::vector<size_t> dont_write_faces;
-    start = clock();
-    orientateMesh(attrib, shapes[0], dont_write_faces);
-    elapsed = clock() - start;
-    std::cout << "Orientating time: " << TIME_TO_MS(elapsed) << " ms\n";
+    start = std::chrono::steady_clock::now();
+    orientate_mesh(attrib, shapes[0], dont_write_faces);
+    stop = std::chrono::steady_clock::now();
+    elapsed = stop - start;
+    std::cout << "Orientating time: " << elapsed.count() << " s\n";
 
-    writeFile(attrib, shapes[0], dont_write_faces, filename);
+    write_file(attrib, shapes[0], dont_write_faces, filename);
     std::cout << "done";
 
     return 0;
